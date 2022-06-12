5c9415e212b2b164addf684adbb0305f28da399c
Nazrawi Demeke <dnazrawi@gmail.com>
2022-06-13 01:54:02 +0300

    minor refactor

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from car_selector import *
 
 if __name__ == '__main__':
-    selector = CarSelector()
-    selection = selector.run()
-
+    selection = CarSelector().run()
     print(f'START GAME {selection}')
 
     # START GAME
